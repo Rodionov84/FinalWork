58dfac8d48d5b6a5264f78a4dfb70428ed5bc09b
Ilya <rodionov84@mail.ru>
2018-04-21 23:59:39 +0300

    Final

--- resources/views/questions_list/questions.blade.php
+++ resources/views/questions_list/questions.blade.php
@@ -1,4 +1,4 @@
-@foreach(App\Question::where('category_id', '=', $category_id)->where('status', '!=', 0)->get() as $question)
+@foreach(App\Question::where('category_id', '=', $category_id)->where('status', '=', 1 )->get() as $question)
     <li>
         <a class="cd-faq-trigger" href="#{{$question->question_id}}">{{$question->question}}</a>
         <div class="cd-faq-content">
